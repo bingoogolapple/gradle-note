c80e25e880785e51e951b27f17f23e1201afb8a9
bingoogolapple <bingoogolapple@gmail.com>
2017-07-09 02:20:56 +0800

    「add」声明 task 的 inputs 和 outputs

---
+++ gn-m1/src/main/java/cn/bingoogol/gradle/m1/Test.java
@@ -0,0 +1,13 @@
+package cn.bingoogol.gradle.m1;
+
+/**
+ * 作者:王浩 邮件:bingoogolapple@gmail.com
+ * 创建时间:2017/7/9 上午1:56
+ * 描述:
+ */
+public class Test {
+
+    public static void test() {
+        System.out.println("test");
+    }
+}
